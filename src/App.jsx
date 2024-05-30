7bd607ec10e4c05044c6d7717ae62812f30354e5
ARUNKUMAR2906 <arunkumar290601@gmail.com>
2024-05-30 19:56:34 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import Calculator from "./components/calculator";
 export default function App() {
   return (
     <div className="">
-      <div className="flex flex-col items-center mt-[12%] sm:mt-[5%] mb-[5%] font-poppins">
+      <div className="flex flex-col items-center mt-[12%] sm:mt-[4%] mb-[4%] font-poppins">
         <h1 className="text-2xl sm:text-4xl text-slate-400 ">Reliable,efficient delivery</h1>
         <h1 className="text-2xl sm:text-4xl font-bold mt-2">Powered by Technology</h1>
-        <div className="mt-6 text-justify sm:text-center text-sm sm:text-xl text-slate-400 pl-[40px] pr-[40px] mb-6">
+        <div className="mt-6 text-justify sm:text-center text-sm sm:text-xl text-slate-400 pl-[40px] pr-[40px] mb-6 sm:mb-0">
         <p>Our Artificial intelligence powered tools use millions of project data <span className="sm:hidden">points to ensure that your project is successful</span></p>
         <p className="hidden sm:block">points to ensure that your project is successful</p>
         </div>
